ca739e20a347b4b3e3452ebb0c83aafbc6f48991
Piotr Maroszek <piotr.maroszek@microsoft.wsei.edu.pl>
2021-02-04 18:47:30 +0100

    added some basic commands to VMMV library and user class to VF library

---
+++ VFViewModel/Commands/LoginCommands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace VFViewModel.Commands
+{
+    public class LoginCommands : ICommand
+    {
+        public LoginVM Vm { get; set; }
+        public event EventHandler CanExecuteChanged;
+
+        public LoginCommands(LoginVM vm)
+        {
+            Vm = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //todo
+        }
+    }
+}
---
+++ VFViewModel/LoginVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VFLibrary;
+using VFViewModel.Commands;
+
+namespace VFViewModel
+{
+    public class LoginVM
+    {
+        private User user;
+
+        public User User
+        {
+            get => user;
+            set { user = value; }
+        }
+        public RegisterCommand RegisterCommand { get; set; }
+        public LoginCommands LoginCommands { get; set; }
+        public LoginVM()
+        {
+            RegisterCommand = new RegisterCommand(this);
+            LoginCommands = new LoginCommands(this);
+        }
+
+    }
+}
