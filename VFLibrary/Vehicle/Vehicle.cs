b5e208caa2fb7d935bac0aa3a4f8f7045104adb4
Piotr Maroszek <piotr.maroszek@microsoft.wsei.edu.pl>
2021-02-07 15:52:21 +0100

    new structore of app files and some brand new windows

---
+++ VFLibrary/Vehicle/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VFLibrary
+{
+    public class Vehicle
+    {
+        [PrimaryKey]
+        public string Vin { get; private set; }
+        public string VehiclePlate { get; set; }
+        public DateTime purchaseDate{ get; private set; }
+        public int InitialKilometersValue { get; }
+        public DateTime RegistrationDate { get; set; }
+        public DateTime SalesDate { get; set; }
+        public int EndKilometersValue { get; set; }
+    }
+}
