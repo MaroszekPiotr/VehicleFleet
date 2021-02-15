e7f793b5613d584747c25755c2f8a9bbb4348163
Piotr Maroszek <piotr.maroszek@microsoft.wsei.edu.pl>
2021-02-15 10:29:38 +0100

    vehicle event list window is complete

---
+++ VFViewModel/Commands/EventCommand/VehicleEventFilterCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using VFViewModel.Event;
+
+namespace VFViewModel.Commands.EventCommand
+{
+    public class VehicleEventFilterCommand : ICommand
+    {
+        private VehiclesEventsVM VehiclesEventsVM { get; set; }
+        public event EventHandler CanExecuteChanged;
+        public VehicleEventFilterCommand(VehiclesEventsVM vehiclesEventsVM)
+        {
+            VehiclesEventsVM = vehiclesEventsVM;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            VehiclesEventsVM.FilterVehicleEventsList();
+        }
+    }
+}
--- VFViewModel/Event/VehiclesEventsVM.cs
+++ VFViewModel/Event/VehiclesEventsVM.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VFLibrary.VehicleEvent;
+using VFViewModel.Commands.EventCommand;
+using VFViewModel.Drivers;
 using VFViewModel.Helpers;
 using VFViewModel.Vehicles;
 
@@ -12,6 +14,9 @@ namespace VFViewModel.Event
 {
     public class VehiclesEventsVM : ListEventWindowHelper
     {
+        #region commands
+        public VehicleEventFilterCommand VehicleEventFilterCommand { get; set; }
+        #endregion
         public ObservableCollection<VehicleHistory> VehiclesEvents { get; set; }
         private VehicleHistory selectedVehicleHistory;
 
@@ -28,17 +33,36 @@ namespace VFViewModel.Event
         public VehiclesEventsVM()
         {
             VehiclesVM = new VehiclesVM();
+            DriversVM = new DriversVM();
             VehiclesEvents = new ObservableCollection<VehicleHistory>();
+            VehicleEventFilterCommand = new VehicleEventFilterCommand(this);
             GetVehicleEvents();
         }
-        public void GetVehicleEvents()
+        public void GetVehicleEvents(string filterVinNumber = "")
         {
-            var vehicleEvents = DatabaseHelper.Read<VehicleHistory>().OrderBy(vehicleEvent=>vehicleEvent.EventTime);
+            IEnumerable<VehicleHistory> vehicleEvents;
+            if (String.IsNullOrEmpty(filterVinNumber))
+            {
+                vehicleEvents = DatabaseHelper.Read<VehicleHistory>().OrderBy(vehicleEvent => vehicleEvent.EventTime);
+            }
+            else
+            {
+
+                vehicleEvents = DatabaseHelper.Read<VehicleHistory>().Where(vehicle => vehicle.Vin == VehiclesVM.SelectedVehicle.Vin).OrderBy(vehicleEvent => vehicleEvent.EventTime);
+            }
             VehiclesEvents.Clear();
-            foreach(var vehicleEvent in vehicleEvents)
+            foreach (var vehicleEvent in vehicleEvents)
             {
                 vehicleEvent.Vehicle = ReadVehicle(vehicleEvent.Vin);
+                if (vehicleEvent.DriverId != 0) vehicleEvent.Driver = ReadDriver(vehicleEvent.DriverId);
                 VehiclesEvents.Add(vehicleEvent);
+            }
+        }
+        public void FilterVehicleEventsList()
+        {
+            if (VehiclesVM.SelectedVehicle != null)
+            {
+                GetVehicleEvents(VehiclesVM.SelectedVehicle.Vin);
             }
         }
     }
