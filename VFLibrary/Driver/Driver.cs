6c419befbb8f1f04f1c6fb0e9115683dd3d456a7
Piotr Maroszek <piotr.maroszek@microsoft.wsei.edu.pl>
2021-02-15 15:29:28 +0100

    Added some fundamental code for driver and vehicle library classes.

--- VFLibrary/Driver/Driver.cs
+++ VFLibrary/Driver/Driver.cs
@@ -13,12 +13,30 @@ namespace VFLibrary
     [Table("Drivers")]
     public class Driver : IEquatable<Driver>
     {
+        /// <summary>
+        /// Identifier for sql use only.
+        /// </summary>
         [PrimaryKey, AutoIncrement]
         public uint Id { get; set; }
+        /// <summary>
+        /// First name of driver.
+        /// </summary>
         public string FirstName { get; set; }
+        /// <summary>
+        /// Last name of driver.
+        /// </summary>
         public string LastName { get; set; }
+        /// <summary>
+        /// Nationality of driver. In some cases it's necessary information because of the law statement.
+        /// </summary>
         public string Nationality { get; set; }
+        /// <summary>
+        /// Important information for drivers who hasn't pesel numbers.
+        /// </summary>
         public string AdditionalId { get; set; }
+        /// <summary>
+        /// Identifier for Polish Drivers.
+        /// </summary>
         private string pesel;
 
         public string Pesel
@@ -31,7 +49,9 @@ namespace VFLibrary
                 else this.pesel = SetPesel(value);*/
             }
         }
-
+        /// <summary>
+        /// Set driver as active or unactive. Default velue in case of create a new driver is active. You can change it every time.
+        /// </summary>
         public bool IsActive { get; set; } = true;
         #region constructors
         /// <summary>
@@ -53,11 +73,31 @@ namespace VFLibrary
             Nationality = nationality;
         }
         #endregion
+        #region equals
         public bool Equals(Driver other)
         {
-            throw new NotImplementedException();
+            if (other == null) return false;
+            if (Object.ReferenceEquals(this, other)) return true;
+            return (this.FirstName == other.FirstName && this.LastName == other.LastName && this.Nationality == other.Nationality && this.AdditionalId == other.AdditionalId && this.pesel == other.pesel);
         }
 
+        public override bool Equals(object obj)
+        {
+            if (obj is Driver) return Equals((Driver)obj);
+            else return false;
+        }
+        public static bool Equals (Driver d1, Driver d2)
+        {
+            if ((d1 is null) && (d2 is null)) return true;
+            if (d1 is null) return false;
+            return d1.Equals(d2);
+        }
+        public override int GetHashCode() => (FirstName, LastName, Nationality, AdditionalId, Pesel).GetHashCode();
+        #endregion
+        #region operators
+        public static bool operator ==(Driver d1, Driver d2) => Equals(d1, d2);
+        public static bool operator !=(Driver d1, Driver d2) => !(Equals(d1, d2));
+        #endregion
         #region methods
         /// <summary>
         /// Method for adding pesel number for drivers with Polish nationality
@@ -69,28 +109,15 @@ namespace VFLibrary
             try
             {
                 if (PeselValidator(pesel)) peselResult = pesel;
-            } catch (ArgumentException)
+            }
+            catch (ArgumentException)
             {
                 Console.WriteLine("Incorrect PESEL!");
             }
             return peselResult;
         }
         /// <summary>
-        /// Set a driver as unactive.
-        /// </summary>
-        public void SetDriverAsArchive()
-        {
-            this.IsActive = true;
-        }
-        /// <summary>
-        /// Set a driver as active.
-        /// </summary>
-        public void SetDriverAsActive()
-        {
-            this.IsActive = false;
-        }
-        /// <summary>
-        /// Static method for validating polish identificate number PESEL, unique for every person with polish nationality
+        /// Static method for validating Polish identificate number PESEL, unique for every person with Polish nationality
         /// </summary>
         /// <param name="peselToCheck"></param>
         /// <returns></returns>
@@ -122,7 +149,19 @@ namespace VFLibrary
 
             return validationResult;
         }
+        /// <summary>
+        /// Parser of string data
+        /// </summary>
+        /// <param name="firstNameLastNameSeparatedByComma">String attribute of frist name and last name separated by comma or space sign</param>
+        /// <returns>Return Driver class</returns>
+        public static Driver Parse(string firstNameLastNameSeparatedByComma)
+        {
+            firstNameLastNameSeparatedByComma = firstNameLastNameSeparatedByComma.Replace(" ", ";").Replace(",", ";");
+            string[] inputData = firstNameLastNameSeparatedByComma.Split(";");
+            if (inputData.Length < 2) return null;
+            return new Driver(inputData[0],inputData[1]);
+        }
         #endregion
-        
+
     }
 }
--- VFLibrary/Vehicle/Vehicle.cs
+++ VFLibrary/Vehicle/Vehicle.cs
@@ -7,24 +7,95 @@ using System.Threading.Tasks;
 
 namespace VFLibrary
 {
+    /// <summary>
+    /// Vehicle class. In this class you can create Vehicle object and attached to it Driver objerct.
+    /// </summary>
     [Table("Vehicles")]
-    public class Vehicle
+    public class Vehicle : IEquatable<Vehicle>
     {
+        /// <summary>
+        /// Most fundamental idefitier of vehicle. It's Vehicle Identification Number and every car has it from manufacture.
+        /// </summary>
         [PrimaryKey]
         public string Vin { get; set; }
+        /// <summary>
+        /// Second important identifier of car.
+        /// </summary>
         [Unique]
         public string VehiclePlate { get; set; }
+        /// <summary>
+        /// It's description for user for easily identify of vehicle. You can set for example manufacture and model name, for example "Tesla Model S, the red one"
+        /// </summary>
         public string VehicleDescription { get; set; }
+        /// <summary>
+        /// set purchase date only if you need it
+        /// </summary>
         public DateTime PurchaseDate{ get; set; }
+        /// <summary>
+        /// set InitialKilometersValue only if you need it
+        /// </summary>
         public int InitialKilometersValue { get; set; }
+        /// <summary>
+        /// set as IsActive(false) in end of usage of vehilce, for example if you sold it
+        /// </summary>
         public bool IsActive { get; set; } = true;
+        /// <summary>
+        /// DriverID is for sql usage only for identify of Driver
+        /// </summary>
         [Indexed]
         public uint DriverID { get; set; }
+        /// <summary>
+        /// Driver assign to vehicle.
+        /// </summary>
         [Ignore]
         public Driver Driver { get; set; }
-
+        #region constructors
+        /// <summary>
+        /// Constructor without parameters for database use only
+        /// </summary>
         public Vehicle()
         {
         }
+        public Vehicle(string vin)
+        {
+            Vin = vin;
+        }
+        public Vehicle(string vin, string vehiclePlate) : this(vin)
+        {
+            VehiclePlate = vehiclePlate;
+        }
+        public Vehicle(string vin, string vehiclePlate, string vehicleDescription) : this(vin,vehiclePlate)
+        {
+            VehicleDescription = vehicleDescription;
+        }
+        public Vehicle(string vin, string vehiclePlate, string vehicleDescription, Driver driver) : this(vin, vehiclePlate, vehicleDescription)
+        {
+            Driver = driver;
+        }
+        #endregion
+        #region equals
+        public bool Equals(Vehicle other)
+        {
+            if (other == null) return false;
+            if (object.ReferenceEquals(this, other)) return true;
+            return (this.Vin == other.Vin && this.VehiclePlate == other.VehiclePlate&&this.VehicleDescription==other.VehicleDescription&&this.PurchaseDate==other.PurchaseDate&&this.InitialKilometersValue==other.InitialKilometersValue&&this.IsActive==other.IsActive&&this.Driver==other.Driver);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Vehicle) return Equals((Vehicle)obj);
+            else return false;
+        }
+        public static bool Equals(Vehicle v1, Vehicle v2)
+        {
+            if ((v1 is null) && (v2 is null)) return true;
+            if (v1 is null) return false;
+            return v1.Equals(v2);
+        }
+        public override int GetHashCode() => (this.Vin, this.VehiclePlate, this.VehicleDescription, this.PurchaseDate, this.InitialKilometersValue,this.IsActive, this.Driver).GetHashCode();
+        #endregion
+        #region operators
+        public static bool operator ==(Vehicle v1, Vehicle v2) => v1.Equals(v2);
+        public static bool operator !=(Vehicle v1, Vehicle v2) => !(v1.Equals(v2));
+        #endregion
     }
 }
